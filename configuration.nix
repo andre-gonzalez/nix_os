1272ee4e15e97310e3d259a0460d1a42ab3fec8a
André Gonzalez <lopescg@gmail.com>
2024-10-18 11:13:42 -0600

    Clean up config and chang boot loader from grub to systemd boot

--- configuration.nix
+++ configuration.nix
@@ -7,351 +7,346 @@
 {
   imports =
     [
-      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/vda";
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
     timeout = 1;
   };
 
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
   networking = {
-    hostName = "nixos"; # Define your hostname.
-    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    hostName = "nixos-vm";
+    networkmanager.enable = true;
     wireless.enable = false;
+    # networkmanager.wifi.backend = "iwd";
     # nameservers = ["1.1.1.1" "1.0.0.1"]
+
+    firewall.enable = false;
+    # firewall.allowedTCPPorts = [ 22 ];
+    # firewall.allowedUDPPorts = [ ... ];
+
   };
 
-  # Set your time zone.
+
   time.timeZone = "America/Mexico_City";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "pt_BR.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "dvorak";
     # useXkbConfig = true; # use xkb.options in tty.
   };
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    # Configure keymap in X11
-    xkb.layout = "us";
-    xkb.variant = "dvorak-intl";
-    xkb.options = "caps:swapescape";
-    displayManager = {
-      startx.enable = true;
+
+  services = {
+
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+
+    # Enable CUPS to print documents.
+    # printing.enable = true;
+
+    xserver = {
+      enable = true;
+      xkb.layout = "us";
+      xkb.variant = "dvorak-intl";
+      xkb.options = "caps:swapescape";
+      displayManager.startx.enable = true;
+
+      windowManager.dwm.enable = true;
+      windowManager.dwm.package = pkgs.dwm.overrideAttrs {
+        src = /home/frank/.config/dwm/laptop-dwm;
+      };
+    };
+
+    # Nvidia proprietary drivers
+    # videoDrivers = [ "nvidia" ];
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
+
+    openssh = {
+      enable = true;
+      ports = [ 1050 ];
+      settings.PermitRootLogin = "no";
+      settings.PasswordAuthentication = false;
+      settings.ClientAliveCountMax = 2;
+      settings.TCPKeepAlive = "no";
+      settings.LogLevel = "VERBOSE";
+      settings.AllowUsers = [ "frank" ];
+      settings.PermitEmptyPasswords = "no";
+      settings.MaxAuthTries = 3;
+      settings.X11Forwarding = false;
+      settings.MaxSessions = 2;
+      settings.AllowTcpForwarding = "no";
+      settings.AllowAgentForwarding = "no";
+    };
+
+    cron = {
+      enable = true;
+      # systemCronJobs = [
+      # ];
+    };
+
+    fail2ban = {
+      enable = false;
+      ignoreIP = [ "127.0.0.1/8" ];
+      bantime = "3600";
+      jails = {
+        findtime = "600";
+      };
+      maxretry = 5;
+    };
+
+    sysstat.enable = true;
+
+    tailscale.enable = true;
+  };
+
+
+  programs = {
+    fish.enable = true;
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # mtr.enable = true;
+
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+    slock.enable = true;
+
+  };
+
+
+  users.users = {
+
+    # Disable root login
+    root.hashedPassword = "!";
+
+    frank = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" ];
+      shell = pkgs.fish;
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINsYIPZvhFhETD4PfqryP/yVpVpRW0bYsrwvPxj5uz/R" ];
+      packages = with pkgs; [
+        firefox
+        fish
+      ];
     };
   };
 
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
-
-  # Enable fish shell
-  programs.fish.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.frank = {
-    isNormalUser = true;
-    home = "/home/frank/";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.fish; # Set default user shell to fish
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINsYIPZvhFhETD4PfqryP/yVpVpRW0bYsrwvPxj5uz/R" ];
-    packages = with pkgs; [
-      firefox
-      tree
-      fish
-      bat
-    ];
-  };
-
-  # Disable root login
-  users.users.root.hashedPassword = "!";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    cron
-    ansible
-    fail2ban
-    pam_tmpdir
-    chkrootkit
-    passwdqc
-    aide
-    sysstat
-    git
-    tailscale
-    # btrfs-assistant
-    # btrfs-auto-snapshot
-    timeshift
-    fastfetch
-    xorg.xinit
-    (st.overrideAttrs (oldAttrs: rec {
-      patches = [
-        # Fetch them directly from `st.suckless.org`
-        (fetchpatch {
-          url = "https://st.suckless.org/patches/alpha/st-alpha-20220206-0.8.5.diff";
-          sha256 = "10gvwnpbjw49212k25pddji08f4flal0g9rkwpvkay56w8y81r22";
-        })
-      ];
-      # configFile = writeText "config.def.h" (builtins.readFile /home/frank/.config/st/config.h);
-      configFile = writeText "config.def.h" (builtins.readFile "${builtins.fetchTarball {
-	   url = "https://github.com/andre-gonzalez/st/archive/main.tar.gz";
-	 }}/config.h");
-      postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
-    }))
-    (dmenu.overrideAttrs (oldAttrs: rec {
-      src = builtins.fetchTarball {
-        url = "https://github.com/andre-gonzalez/dmenu/archive/main.tar.gz";
-      };
-    }))
-    (dwmblocks.overrideAttrs (oldAttrs: rec {
-      # configFile = writeText "config.def.h" (builtins.readFile /home/frank/.config/dwmblocks/config.h);
-      configFile = writeText "config.def.h" (builtins.readFile "${builtins.fetchTarball {
-	   url = "https://github.com/andre-gonzalez/dwmblocks/archive/main.tar.gz";
-	 }}/config.h");
-      postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
-    }))
-    lf
-    slock
-    tmux
-    tmuxp
-    xautolock
-    xbindkeys
-    arandr
-    numlockx
-    feh
-    unclutter
-    xclip
-    brightnessctl
-    ripgrep
-    fzf
-    lxsession # polkit agent to fill password when launching applications with my run launcher
-    flameshot
-    bat
-    dunst
-    qt5ct # To set theme for qt applications
-    csvkit
-    yamllint
-    ansible-lint
-    nodejs_22 # Necessary to install gramarly language server in neovim play
-    qutebrowser
-    unzip
-    mpv # Terminal based video player
-    yt-dlp # Download youtube videos, necessary to play youtube videos on mpv
-    pre-commit # To use with git to ensure format and linting before commits
-    xorg.xinput
-    pipenv
-    gnumake
-    util-linux
-    liberation_ttf
-    fd
-    libnotify
-    pamixer
-    nerdfonts
-    joypixels
-    libertine
-    arc-theme
-    clipmenu
-    noto-fonts
-    bleachbit
-    awscli
-    python312Packages.debugpy
-    spotify
-    trash-cli
-    nethogs
-    man-db
-    tldr
-    zoxide
-    wol
-    redshift
-    inetutils
-    ntfs3g
-    newsboat
-    python312Packages.adblock
-    libreoffice-qt6-still
-    noto-fonts-color-emoji
-    ncdu
-    delta
-    brave
-    dwm-status
-    stw
-    anki-bin
-    rsync
-    iwd
-    zathura
-    obsidian
-    sent
-    farbfeld
-    preload
-    tlp
-    smartmontools
-    ethtool
-    powertop
-    docker
-    docker-compose
-    dbeaver-bin
-    xcompmgr
-    nil # Nix language server
-    nixpkgs-fmt # Nix formatter
-    home-manager # Nix tool to manage home environment
-  ];
+  environment = {
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
+    extraInit = "umask 027";
+
+    systemPackages = with pkgs; [
+      # Base packages
+      # aide
+      # chkrootkit
+      cron
+      # ethtool
+      fail2ban
+      git
+      # inetutils
+      # iwd
+      libgcc
+      # man-db
+      neovim
+      # nethogs
+      # ntfs3g
+      # pam_tmpdir
+      # passwdqc
+      # rsync
+      # smartmontools
+      sysstat
+      # tldr
+      # trash-cli
+      unzip
+      util-linux
+
+      # Packages to develop on nix files
+      home-manager
+      nil
+      nixpkgs-fmt
+
+      # Develop Packages
+      ansible
+      # ansible-lint
+      bat
+      # csvkit
+      # dbeaver-bin
+      # delta
+      # docker
+      # docker-compose
+      # fd
+      fzf
+      # lf
+      # pipenv
+      # pre-commit
+      # python312Packages.debugpy
+      ripgrep
+      tailscale
+      tmux
+      tmuxp
+      # tree
+      # yamllint
+      # zoxide
+
+      # Random Packages
+      fastfetch
+
+      # BTRFS Packages
+      # btrfs-assistant
+      # btrfs-auto-snapshot
+      # timeshift
+
+      # Graphical environment
+      arandr
+      arc-theme
+      brightnessctl
+      clipmenu
+      # dunst
+      dwm-status
+      feh
+      # flameshot
+      # lxsession # polkit agent to fill password when launching applications with my run launcher
+      numlockx
+      pamixer
+      # qt5ct # To set theme for qt applications
+      redshift
+      slock
+      stw
+      # unclutter
+      xautolock
+      xbindkeys
+      xclip
+      # xcompmgr
+      xorg.xinit
+      # (st.overrideAttrs (oldAttrs: rec {
+      #   patches = [
+      #     # Fetch them directly from `st.suckless.org`
+      #     (fetchpatch {
+      #       url = "https://st.suckless.org/patches/alpha/st-alpha-20220206-0.8.5.diff";
+      #       sha256 = "10gvwnpbjw49212k25pddji08f4flal0g9rkwpvkay56w8y81r22";
+      #     })
+      #   ];
+      #   # configFile = writeText "config.def.h" (builtins.readFile /home/frank/.config/st/config.h);
+      #   configFile = writeText "config.def.h" (builtins.readFile "${builtins.fetchTarball {
+      #   url =  "https://github.com/andre-gonzalez/st/archive/main.tar.gz";
+      #   }}/con fig.h");
+      #   postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
+      # }))
+      (dmenu.overrideAttrs (oldAttrs: rec {
+        src = builtins.fetchTarball {
+          url = "https://github.com/andre-gonzalez/dmenu/archive/main.tar.gz";
+        };
+      }))
+      (dwmblocks.overrideAttrs (oldAttrs: rec {
+        # configFile = writeText "config.def.h" (builtins.readFile /home/frank/.config/dwmblocks/config.h);
+        configFile = writeText "config.def.h" (builtins.readFile "${builtins.fetchTarball {
+url = "https://github.com/andre-gonzalez/dwmblocks/archive/main.tar.gz";
+}}/config.h");
+        postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
+      }))
+
+      # Fonts
+      # joypixels
+      # libertine
+      # nerdfonts
+      # noto-fonts
+      # noto-fonts-color-emoji
+
+      # Web Browser
+      # brave
+      # qutebrowser
+
+      # Workstation
+      # bleachbit
+      # farbfeld
+      # libreoffice-qt6-still
+      # mpv
+      # ncdu
+      # newsboat
+      # obsidian
+      # preload
+      # python312Packages.adblock
+      # sent
+      # spotify
+      # yt-dlp
+      # zathura
+
+      # Laptop
+      # powertop
+      # tlp
+
+    ];
+
+  };
+
+
+  system = {
+    # Copy the NixOS configuration file and link it from the resulting system
+    # (/run/current-system/configuration.nix). This is useful in case you
+    # accidentally delete configuration.nix.
+    copySystemConfiguration = true;
+    # This option defines the first version of NixOS you have installed on this particular machine,
+    # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+    #
+    # Most users should NEVER change this value after the initial install, for any reason,
+    # even if you've upgraded your system to a new NixOS release.
+    #
+    # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+    # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+    # to actually do that.
+    #
+    # This value being lower than the current NixOS release does NOT mean your system is
+    # out of date, out of support, or vulnerable.
+    #
+    # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+    # and migrated your data accordingly.
+    #
+    # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+    stateVersion = "24.05"; # Did you read the comment?
+  };
+
+
+  qt = {
     enable = true;
-    enableSSHSupport = true;
+    # Make QT applications look similar to GTK ones
+    platformTheme = "gtk2";
+    style = "gtk2";
   };
 
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    ports = [ 1050 ];
-    settings.PermitRootLogin = "no";
-    settings.PasswordAuthentication = false;
-    settings.ClientAliveCountMax = 2;
-    settings.TCPKeepAlive = "no";
-    settings.LogLevel = "VERBOSE";
-    settings.AllowUsers = [ "frank" ];
-    settings.PermitEmptyPasswords = "no";
-    settings.MaxAuthTries = 3;
-    settings.X11Forwarding = false;
-    settings.MaxSessions = 2;
-    settings.AllowTcpForwarding = "no";
-    settings.AllowAgentForwarding = "no";
+  nixpkgs.config = {
+
+    # Allow unfree packages
+    allowUnfree = true;
+
+    joypixels.acceptLicense = true;
+
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-  # Nvidia proprietary drivers
-  # services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Make QT applications look similar to GTK ones
-  qt.enable = true;
-  qt.platformTheme = "gtk2";
-  qt.style = "gtk2";
-
-  # Configure cron
-  services.cron = {
-    enable = true;
-    # systemCronJobs = [
-    # ];
-  };
-
-  # Configure fail2ban
-  services.fail2ban = {
-    enable = false;
-    ignoreIP = [ "127.0.0.1/8" ];
-    bantime = "3600";
-    jails = {
-      findtime = "600";
-    };
-    maxretry = 5;
-  };
 
   # Disable core dumps
   systemd.coredump.enable = false;
 
-  # Set umask to 027
-  environment.extraInit = "umask 027";
-
-  # Enable sysstat
-  services.sysstat.enable = true;
-
-  # Enable tailscale
-  services.tailscale.enable = true;
-
-  # Set neovim as default editor
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
-
-  # Configure git THIS CONFIGURATION SHOULD BE MOVED TO HOME MANAGER https://nixos.wiki/wiki/Home_Manager
-  # programs.git = {
-  #   enable = true;
-  #   userName = "André Gonzalez";
-  #   userEmail = "lopescg@gmail.com";
-  # aliases = {
-  #   ga = "add";
-  #   gc = "commit";
-  #   gp = "push";
-  #   gs = "status";
-  # };
-  # };
-
-  # Configure home manager to create directories
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable dwm
-  services.xserver.windowManager.dwm.enable = true;
-  services.xserver.windowManager.dwm.package = pkgs.dwm.overrideAttrs {
-    src = /home/frank/.config/dwm;
-  };
-
-  # Accept joypixels license
-  nixpkgs.config.joypixels.acceptLicense = true;
-
-  # Enable slock
-  programs.slock.enable = true;
 }
