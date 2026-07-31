c135e71bedc9d7dd31237886f3fb7e95d16bc0f0
André Gonzalez <lopescg@gmail.com>
2026-07-31 16:07:20 -0300

    Add mpris-proxy service enablement

--- modules/nixos/desktop/bluetooth.nix
+++ modules/nixos/desktop/bluetooth.nix
@@ -8,5 +8,13 @@
 
   services.blueman.enable = true;
 
+  # Bridge Bluetooth AVRCP (headset play/pause/next buttons) to MPRIS players.
+  systemd.user.services.mpris-proxy = {
+    description = "Forward bluetooth media controls to MPRIS";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
+
   environment.systemPackages = [ pkgs.bluez ];
 }
